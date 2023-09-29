1cbb1de78752bfa7bd5806157023b613b95dbcb9
veelcheck <aga_wilczek@onet.eu>
2023-09-29 10:01:23 +0200

    final version v1

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const displayValid = document.getElementById('js-return-valid');
 const displayInvalid = document.getElementById('js-return-invalid');
 const displayWarning = document.getElementById('js-warning-not-number');
 const displayWarningLength = document.getElementById('js-warning-not-length');
-const inputElement = document.getElementById("userInput");
+const inputElement = document.getElementById('js-userInput');
+const buttonElement = document.getElementById('js-button');
 
 let input;
 let validTimeoutId;
@@ -13,80 +14,89 @@ let newTotal;
 let allDigitsArray;
 let company;
 
+function doItAll(input) {
+  input = inputElement.value;
+  const inputInteger = Number(input); //To make sure the input is only numbers.
+
+  if (!inputInteger) {
+    displayWarning.style.display = "block";
+    displayWarning.innerHTML = `Check again.<br> Your input is not a number.`;
+    displayInvalid.style.display = "none";
+    displayValid.style.display = "none";
+    displayWarningLength.style.display = 'none';
+    warningNumTimeoutId = setTimeout(() => {
+      displayWarning.style.display = 'none';
+      inputElement.value = null;
+  }, 4000);
+    return;
+  } 
+
+  if (input.length < 13 || input.length > 19) {
+    displayWarningLength.style.display = "block";
+    displayWarningLength.innerHTML = `No, no, no, no.<br>Enter between 13 and 19 characters, please.`;
+    displayWarning.style.display = 'none';
+    displayInvalid.style.display = "none";
+    displayValid.style.display = "none";
+    warningLengthTimeoutId = setTimeout(() => {
+      displayWarningLength.style.display = 'none';
+      inputElement.value = null;
+  }, 4000);
+    return;
+  }
+  
+  input = String(input).split('').map((input
+  ) => { return Number(input) });
+
+  checkCompany(input);
+  
+  
+
+  displayValid.style.display = "block";
+  displayInvalid.style.display = "block";
+
+  clearTimeout(validTimeoutId);
+  clearTimeout(invalidTimeoutId);
+  clearTimeout(warningNumTimeoutId);
+  clearTimeout(warningLengthTimeoutId);  
+
+  if (!validateCred(input)) { 
+    
+    makeItValid(input);
+
+    displayInvalid.innerHTML = `This number is not valid.<br>Try this ${newTotal}.`;
+    displayValid.style.display = "none";
+    displayWarning.style.display = 'none';
+    displayWarningLength.style.display = 'none';
+    invalidTimeoutId = setTimeout(() => {
+          displayInvalid.style.display = 'none';
+          inputElement.value = null;
+      }, 9000); 
+
+  } else {
+    
+      displayValid.innerHTML = `<img src="images/${company}.png">Good. This number is valid.<br>Happy shopping!`;
+      displayInvalid.style.display = "none";
+      displayWarning.style.display = 'none';
+      displayWarningLength.style.display = 'none';
+      validTimeoutId = setTimeout(function() {
+            displayValid.style.display = "none";
+            inputElement.value = null;
+        }, 9000);
+    }
+}
+
     function handleEnter() {
       inputElement.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-          input = inputElement.value;
-          const inputInteger = Number(input);
-
-          if (!inputInteger) {
-            displayWarning.style.display = "block";
-            displayWarning.innerHTML = `Check again.<br> Your input is not a number.`;
-            displayInvalid.style.display = "none";
-            displayValid.style.display = "none";
-            displayWarningLength.style.display = 'none';
-            warningNumTimeoutId = setTimeout(() => {
-              displayWarning.style.display = 'none';
-              inputElement.value = null;
-          }, 4000);
-            return;
-          } 
-
-          if (input.length < 13 || input.length > 19) {
-            displayWarningLength.style.display = "block";
-            displayWarningLength.innerHTML = `No, no, no, no.<br>Enter between 13 and 19 characters, please.`;
-            displayWarning.style.display = 'none';
-            displayInvalid.style.display = "none";
-            displayValid.style.display = "none";
-            warningLengthTimeoutId = setTimeout(() => {
-              displayWarningLength.style.display = 'none';
-              inputElement.value = null;
-          }, 4000);
-            return;
-          }
-          
-          input = String(input).split('').map((input
-          ) => { return Number(input) });
-
-          checkCompany(input);
-          
-          
-
-          displayValid.style.display = "block";
-          displayInvalid.style.display = "block";
-  
-          clearTimeout(validTimeoutId);
-          clearTimeout(invalidTimeoutId);
-          clearTimeout(warningNumTimeoutId);
-          clearTimeout(warningLengthTimeoutId);  
-  
-          if (!validateCred(input)) { 
-            
-            makeItValid(input);
-  
-            displayInvalid.innerHTML = `This number is not valid.<br>Try this ${newTotal}.`;
-            displayValid.style.display = "none";
-            displayWarning.style.display = 'none';
-            displayWarningLength.style.display = 'none';
-            invalidTimeoutId = setTimeout(() => {
-                  displayInvalid.style.display = 'none';
-                  inputElement.value = null;
-              }, 9000); 
-  
-          } else {
-            
-              displayValid.innerHTML = `<img src="images/${company}.png">Good. This number is valid.<br>Happy shopping!`;
-              displayInvalid.style.display = "none";
-              displayWarning.style.display = 'none';
-              displayWarningLength.style.display = 'none';
-              validTimeoutId = setTimeout(function() {
-                    displayValid.style.display = "none";
-                    inputElement.value = null;
-                }, 9000);
-            }
+          doItAll(input);
         } 
       })
-      
+    }
+
+    function handleClick() {
+      buttonElement.addEventListener('click', function() {
+        doItAll(input);
+      })
     }
     
  
@@ -184,6 +194,7 @@ let company;
     }
 
     handleEnter();
+    handleClick();
 
     function checkCompany (num) {
       const twoDigits = num.slice(0, 2).join('');
